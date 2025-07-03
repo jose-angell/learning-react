1835c4b0ae54fb446dba8d6201529f4428d97eff
jose Angel <josegallardo.cor@gmail.com>
2025-07-02 20:52:22 -0600

    uso de dos useEffect

--- proyects/04-react-prueba-tecnica/src/App.jsx
+++ proyects/04-react-prueba-tecnica/src/App.jsx
@@ -1,33 +1,46 @@
 import {useState, useEffect} from 'react';
-
+import './App.css'
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
 
 export function App () {
 
     const [fact, setFact] = useState()
-    const [imageUrl, setImageUrl] = useState()
+    const [imageUrl, setImageUrl] = useState();
+    // para recuperar  la cita al cargar la app
     useEffect(() => {
         fetch(CAT_ENDPOINT_RANDOM_FACT)
         .then(res => res.json())
         .then(data => {
             const {fact} = data
-            setFact(data.fact)
-
-            const firsThreeWord = fact.split(' ', 3).join(' ');
-            fetch(`https://cataas.com/cat/says/${firsThreeWord}?size=50&color=red&json=true`)
-            .then(res => res.json())
-            .then(response => {
-                const {url} = response;
+            setFact(fact)
+           // const firsThreeWord = fact.split(' ', 3).join(' ');
+           // fetch(`https://cataas.com/cat/says/${firsThreeWord}?size=50&color=red&json=true`)
+           // .then(res => res.json())
+           // .then(response => {
+           //     const {url} = response;
                 //console.log(url);
-                setImageUrl(url);
-            })
+           //     setImageUrl(url);
+           // })
+            
         })
-    }, [])
+    }, []);
+    // para recuperar la imagen al cambiar la cita
+    useEffect(() => {
+        if(!fact) return;
+        const firsThreeWord = fact.split(' ', 3).join(' ');
+        fetch(`https://cataas.com/cat/says/${firsThreeWord}?size=50&color=red&json=true`)
+        .then(res => res.json())
+        .then(response => {
+            const {url} = response;
+            //console.log(url);
+            setImageUrl(url);
+        })
+    }, [fact])
     return (
         <main>
             <h1>App de gatos</h1>
             {fact && <p>{fact}</p>}
-            {imageUrl && <img src={imageUrl} alt={`Image extracted using the first trhee words for ${fact}`} width="300px" />}
+            {imageUrl && <img src={imageUrl} alt={`Image extracted using the first trhee words for ${fact}`}  />}
         </main>
     )
 }
