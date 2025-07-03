9582b31b398a3cc2b5d3cdf94c8539487c676386
jose Angel <josegallardo.cor@gmail.com>
2025-07-02 20:29:16 -0600

    prueba en estado inicial completo

--- proyects/04-react-prueba-tecnica/src/App.jsx
+++ proyects/04-react-prueba-tecnica/src/App.jsx
@@ -5,17 +5,29 @@ const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
 export function App () {
 
     const [fact, setFact] = useState()
+    const [imageUrl, setImageUrl] = useState()
     useEffect(() => {
-        async function getRandomFact () {
-            const rest = await fetch(CAT_ENDPOINT_RANDOM_FACT)
-            const json = await  res.json()
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then(res => res.json())
+        .then(data => {
+            const {fact} = data
             setFact(data.fact)
-        }
+
+            const firsThreeWord = fact.split(' ', 3).join(' ');
+            fetch(`https://cataas.com/cat/says/${firsThreeWord}?size=50&color=red&json=true`)
+            .then(res => res.json())
+            .then(response => {
+                const {url} = response;
+                //console.log(url);
+                setImageUrl(url);
+            })
+        })
     }, [])
     return (
         <main>
             <h1>App de gatos</h1>
             {fact && <p>{fact}</p>}
+            {imageUrl && <img src={imageUrl} alt={`Image extracted using the first trhee words for ${fact}`} width="300px" />}
         </main>
     )
 }
